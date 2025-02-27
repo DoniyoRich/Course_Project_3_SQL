7c29672011841f9f054c65feebb01450b8006826
Doniyor <donish1979@gmail.com>
2025-02-27 22:32:18 +0300

    initial commit for feature/db_hh branch

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import os
 from dotenv import load_dotenv
 
+from src.DBManagerClass import DBManager
+
 load_dotenv()
 
 
 def main():
     api_key_hh = os.getenv('API_KEY_HH')
     print(api_key_hh)
+
+    hh = DBManager()
 
 
 if __name__ == '__main__':
